f68630c6bc24c3ada313265a57e612bcecdd7f08
lalit <lk945600@gmail.com>
2024-12-25 10:35:28 +0530

    minor changes

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,8 @@
 import '../styles/globals.css'; // Next.js example
-// import { Amplify } from 'aws-amplify';
-// import awsExports from '../aws-exports'; // Adjust path based on where aws-exports.js resides
+import { Amplify } from 'aws-amplify';
+import awsExports from '../aws-exports'; // Adjust path based on where aws-exports.js resides
 
-// Amplify.configure(awsExports);
+Amplify.configure(awsExports);
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
